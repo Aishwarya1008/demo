89413fcc29edfc03b02b02a77aa8351adb5863af
Aishwarya1008 <aishwarya.rohatgi1008@gmail.com>
2022-01-18 16:56:29 +0530

    create-user

---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+
+@RestController
+public class UserController {
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/user")
+	String getUser(){
+		System.out.println("testing");
+		return "testing";
+	}
+	
+	@PostMapping("/user")
+	void createUser(@RequestBody User user) {
+		System.out.println(user.getName());
+		userService.saveUser(user);
+	}
+}
