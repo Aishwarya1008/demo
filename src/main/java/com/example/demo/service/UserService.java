78f0a199579d7c34e4b11f8f77c5766ca42f670b
Aishwarya1008 <aishwarya.rohatgi1008@gmail.com>
2022-01-20 15:45:31 +0530

    Validatin added

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
@@ -14,5 +15,14 @@ public class UserService {
 	
 	public void saveUser(User user) {
 		userRepository.save(user);
+	}
+
+	public Iterable<User> getUsers() {
+		return userRepository.findAll();
+	}
+
+	public Optional<User> getUser(Integer id) {
+		
+		return userRepository.findById(id);
 	}
 }
